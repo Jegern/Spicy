1a544b006c37e858ae20132745e3a5b409709b45
“Jegern” <severuanov2001@mail.ru>
2021-05-07 17:08:46 +0500

    Был разработан макет нового дизайна для приложения

--- MainForm.xaml.cs
+++ MainForm.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Spicy
 {
@@ -7,6 +9,43 @@ namespace Spicy
         public MainForm()
         {
             InitializeComponent();
+            InitializeOtherComponent();
+        }
+
+        private void InitializeOtherComponent()
+        {
+            MinimizeMenu(SfxGrid);
+            MinimizeMenu(TemplateGrid);
+        }
+
+        private void TemplateMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MinimizeMenu(SfxGrid);
+            ExpandMenu(TemplateGrid);
+        }
+
+        private void SfxMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MinimizeMenu(TemplateGrid);
+            ExpandMenu(SfxGrid);
+        }
+
+        void MinimizeMenu(ColumnDefinition grid)
+        {
+            if (grid.Width != new GridLength(0))
+            {
+                grid.Width = new GridLength(0);
+                Width -= 225;
+            }
+        }
+
+        void ExpandMenu(ColumnDefinition grid)
+        {
+            if (grid.Width == new GridLength(0))
+            {
+                grid.Width = new GridLength(0.75, GridUnitType.Star);
+                Width += 225;
+            }
         }
     }
 }
